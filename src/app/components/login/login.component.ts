a22e73c842bfbd0e310af08b70a8d4471354db5a
JeryBan <jerybananas@gmail.com>
2024-08-31 00:07:11 +0300

    added formgroup for login service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,26 +1,41 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {LoginService} from "../../shared/services/login.service";
 import {take} from "rxjs";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {UserLogin} from "../../shared/interfaces/user";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   router: Router = inject(Router)
   loginService: LoginService = inject(LoginService);
-  activeUser = this.loginService.activeUser;
+  formBuilder: FormBuilder = inject(FormBuilder);
 
-  login(username: string) {
-    const body = {username: username, password: '123'}
-    this.loginService.loginUser(body).pipe(take(1)).subscribe({
+  activeUser = this.loginService.activeUser;
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['123'],
+    })
+  }
+
+  login() {
+    const userLogin = this.loginForm?.value as UserLogin;
+    this.loginService.loginUser(userLogin).pipe(take(1)).subscribe({
       next: data => {
         localStorage.setItem('access_token', data.token)
         if (!this.activeUser()) {
+            let username = this.loginForm!.get('username')!.value;
             this.activeUser.set({username: username, is_dm: true})
         }
         this.router.navigate(['my-campaigns']);
